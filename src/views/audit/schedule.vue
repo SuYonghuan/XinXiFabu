a0355716c4d0c988c7d902bbf41f730b39d7ebb7
jiannibang <jiannibang@gmail.com>
2021-04-14 15:40:26 +0800

    日程审核头部未对齐

--- src/views/audit/schedule.vue
+++ src/views/audit/schedule.vue
@@ -2,7 +2,7 @@
   ><table-page>
     <template v-slot:header>
       <el-row type="flex" justify="space-between">
-        <el-col>
+        <el-col style="display:flex">
           <span class="meta1">日程名称</span>
           <el-input
             class="input1"
