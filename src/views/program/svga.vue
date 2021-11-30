187ec237b35785510eb9c7014ab57e63feab48d7
jiannibang <jiannibang@gmail.com>
2021-11-30 17:21:28 +0800

    修复svga无法多个同时播放的问题

--- src/views/program/svga.vue
+++ src/views/program/svga.vue
@@ -4,11 +4,10 @@
 
 <script>
 import { Parser, Player } from "svga";
-const parser = new Parser();
 export default {
   props: ["src"],
   data() {
-    return { player: null };
+    return { parser: null, player: null };
   },
   watch: {
     src(value) {
@@ -16,19 +15,23 @@ export default {
     },
   },
   mounted() {
+    this.parser = new Parser();
     this.player = new Player({
       container: this.$refs.canvas,
       loop: 0,
     });
+
     if (this.src) this.play(this.src);
   },
   beforeDestroy() {
     this.player.destroy();
     this.player = null;
+    this.parser.destroy();
+    this.parser = null;
   },
   methods: {
     async play(src) {
-      const svga = await parser.load(src);
+      const svga = await this.parser.load(src);
       await this.player.mount(svga);
       this.player.start();
     },
