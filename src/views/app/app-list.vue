0618f1495e97299a3007a9d5e7f72702331202b7
昵称为空 <568202184@qq.com>
2021-12-31 15:11:20 +0800

    应用发布按钮删除

--- src/views/app/app-list.vue
+++ src/views/app/app-list.vue
@@ -45,12 +45,12 @@
           </el-link>
         </template>
       </el-table-column>
-      <el-table-column label="操作">
+      <!-- <el-table-column label="操作">
         <template slot-scope="scope">
           <el-button type="primary" size="small" @click="handleEditDevice(scope.row)" v-if="pageMenu.appdevlist">发布
           </el-button>
         </template>
-      </el-table-column>
+      </el-table-column> -->
     </el-table>
 
     <!--  分页  -->
